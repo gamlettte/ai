e0c38d10ba70d8f21d3ef8d1c2ff4e0b9ce793cd
Denys Kurishchenko <denys.kurishchenko@ringteam.com>
2024-04-30 00:03:02 +0300

    Final lab 2 commit

--- lab_2/lab_2.lua
+++ lab_2/lab_2.lua
@@ -7,6 +7,28 @@ local neuron_demo = {}
 neuron_demo.__index = neuron_demo
 
 
+---@private
+---@param weight_vector number[]
+---@param bias number
+---@param activation_f fun(input: number): number
+---@return neuron_demo
+---@nodiscard
+function neuron_demo.do_new(weight_vector, bias, activation_f)
+
+    ---@type neuron_demo
+    local self = setmetatable({
+        _weight_vector = weight_vector,
+        _bias = bias,
+        _input_slots = #weight_vector,
+        _activation_f = activation_f
+    }, neuron_demo)
+
+    return self
+
+end
+
+
+
 ---@public
 ---@param weight_vector number[]
 ---@param bias number
@@ -26,15 +48,35 @@ function neuron_demo.new(weight_vector, bias, activation_f)
     assert(type(activation_f) == "function", type(activation_f))
     assert(type(activation_f(0.0)) == "number")
 
-    ---@type neuron_demo
-    local self = setmetatable({
-        _weight_vector = weight_vector,
-        _bias = bias,
-        _input_slots = #weight_vector,
-        _activation_f = activation_f
-    }, neuron_demo)
+    return neuron_demo.do_new(weight_vector, bias, activation_f)
+end
 
-    return self
+
+---@private
+---@param input number[]
+---@return number
+---@nodiscard
+function neuron_demo:do_run(input)
+
+    print("\nnew case:")
+    print("input   = "..table.concat(input, " "))
+    print("weights = "..table.concat(self._weight_vector, " "))
+
+    ---@type number
+    local sum = self._bias * 1
+    print("bias = "..self._bias)
+
+    for i = 1, #input do
+        print (input[i].." * "..self._weight_vector[i].." = "..input[i] * self._weight_vector[i])
+        sum = sum + input[i] * self._weight_vector[i]
+    end
+    print("final sum = "..sum)
+
+    ---@type number
+    local final_value = self._activation_f(sum)
+    print("final value = "..final_value)
+
+    return final_value
 end
 
 
@@ -44,19 +86,14 @@ end
 ---@nodiscard
 function neuron_demo:run(input)
 
+    assert(input)
     assert(type(input) == "table")
     for _, v in ipairs(input) do
         assert(type(v) == "number")
     end
     assert(#input == #self._weight_vector)
 
-    ---@type number
-    local sum = self._bias
-    for i = 1, #input do
-        sum = sum + input[i] * self._weight_vector[i]
-    end
-
-    return self._activation_f(sum)
+    return self:do_run(input)
 end
 
 
@@ -68,7 +105,9 @@ local bias = -1
 
 ---@type fun(input: number): number
 local function re_lu(input)
+
     assert(type(input) == "number")
+
     return input > 0 and input or 0
 end
 
@@ -84,7 +123,7 @@ local all_possible_inputs = {
 }
 
 ---@type number[]
-local expected_outputs = {
+local and_neuron_expected_outputs = {
     0,
     0,
     0,
@@ -99,9 +138,9 @@ for _, value in ipairs(all_possible_inputs) do
     table.insert(real_results, result)
 end
 
-assert(#real_results == #expected_outputs)
+assert(#real_results == #and_neuron_expected_outputs)
 for i = 1, #real_results do
-    assert(real_results[i] == expected_outputs[i],
-           real_results[i].." is not "..expected_outputs[i])
+    assert(real_results[i] == and_neuron_expected_outputs[i],
+           real_results[i].." is not "..and_neuron_expected_outputs[i])
 end
 
